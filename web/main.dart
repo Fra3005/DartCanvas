177f54ef42ffec7358f01b6d57593975e05483a1
Fra3005 <francescodedeoo@gmail.com>
2022-11-08 22:32:03 +0100

    add httprequest

--- web/main.dart
+++ web/main.dart
@@ -1,20 +1,35 @@
 import 'dart:html';
+import 'package:csv/csv.dart';
+
+
+
+
+
 
 void main() {
   var canvas = document.querySelector('canvas') as CanvasElement ;
   var ctx = canvas.getContext('2d') as CanvasRenderingContext2D ;
+  List<List<dynamic>>? provaTesto; //Dynamic list to contain csv
 
 
+//read csv and convert into a list of list
+  void readCsvSleepFile(String file){
+   provaTesto = CsvToListConverter().convert(file, fieldDelimiter: ';');
+  }
 
 
-  ctx
-    ..strokeStyle = "black"
-    ..beginPath()
-    ..moveTo(100, 100)
-    ..lineTo(150, 150)
-    ..stroke()
-    ..stroke()
-    ..closePath()
-  ;
+//path csv
+ String file = "/assets/prova.csv";
+
+
+//HttpRequest
+  HttpRequest.getString(file).then((String fileContents) {
+  readCsvSleepFile(fileContents);
+  print(provaTesto);
+}).catchError((error) {
+  print(error.toString());
+});
+
+
 
 }
