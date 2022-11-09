61eaadd302346ad4ffb5f83a33ed47fa70cd2c78
Fra3005 <francescodedeoo@gmail.com>
2022-11-09 20:25:39 +0100

    Add some features

--- web/main.dart
+++ web/main.dart
@@ -5,19 +5,25 @@ import 'package:csv/csv.dart';
 var canvas = document.querySelector('canvas') as CanvasElement ;
 var ctx = canvas.getContext('2d') as CanvasRenderingContext2D ;
 List<List<dynamic>> provaTesto = []; //Dynamic list to contain csv
-
+var newEl = [];
 
  void readCsvSleepFile(String file){
    provaTesto = const CsvToListConverter().convert(file);
-   int i = 15;
-
+   num y = 15;
+   num x = 5;
    provaTesto.forEach((element) =>{ 
     
     element.forEach((element) =>{
-      
-    
-     drawLine(5, i, element.length),
-     i =i + 20
+     //drawLine(5, i, element.length),
+     
+     newEl = element.split(" "),
+     x = 5,
+     for (int i=0; i<newEl.length; i++){
+      drawLine(x, y, newEl[i].length),
+      x =x + newEl[i].length + 10, 
+      print(newEl[i].length)
+     }, 
+     y =y + 20, 
     })
   });
  }
@@ -40,7 +46,7 @@ ctx.lineWidth = 4;
 void main() {
  
 //path csv
- String file = "/assets/prova (1).csv";
+ String file = "/assets/Alice.csv";
 
 //HttpRequest
   var request = HttpRequest.getString(file).then(readCsvSleepFile);
