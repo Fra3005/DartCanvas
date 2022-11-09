61250f95053a98e9d8dd377e442a8f1d1021b0f3
Fra3005 <francescodedeoo@gmail.com>
2022-11-09 19:21:38 +0100

    Adding line for each line

--- web/main.dart
+++ web/main.dart
@@ -2,34 +2,47 @@ import 'dart:html';
 import 'package:csv/csv.dart';
 
 
+var canvas = document.querySelector('canvas') as CanvasElement ;
+var ctx = canvas.getContext('2d') as CanvasRenderingContext2D ;
+List<List<dynamic>> provaTesto = []; //Dynamic list to contain csv
 
 
+ void readCsvSleepFile(String file){
+   provaTesto = const CsvToListConverter().convert(file);
+   int i = 15;
+
+   provaTesto.forEach((element) =>{ 
+    
+    element.forEach((element) =>{
+      
+    
+     drawLine(5, i, element.length),
+     i =i + 20
+    })
+  });
+ }
+
+
+void drawLine(x, y, len){
+
+ctx.lineWidth = 4;
+    ctx
+      ..beginPath()
+      ..moveTo(x, y)
+      ..lineTo(x + len, y)
+      ..lineCap = 'round'
+      ..stroke()
+      ..closePath()
+    ;
+}
 
 
 void main() {
-  var canvas = document.querySelector('canvas') as CanvasElement ;
-  var ctx = canvas.getContext('2d') as CanvasRenderingContext2D ;
-  List<List<dynamic>>? provaTesto; //Dynamic list to contain csv
-
-
-//read csv and convert into a list of list
-  void readCsvSleepFile(String file){
-   provaTesto = CsvToListConverter().convert(file, fieldDelimiter: ';');
-  }
-
-
+ 
 //path csv
- String file = "/assets/prova.csv";
-
+ String file = "/assets/prova (1).csv";
 
 //HttpRequest
-  HttpRequest.getString(file).then((String fileContents) {
-  readCsvSleepFile(fileContents);
-  print(provaTesto);
-}).catchError((error) {
-  print(error.toString());
-});
-
-
+  var request = HttpRequest.getString(file).then(readCsvSleepFile);
 
 }
